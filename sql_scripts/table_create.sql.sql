c3d7192364fa6e7dd27ec222f31f9aa4c5d2b56f
SaadNasir92 <nas.saad53@gmail.com>
2024-07-10 19:57:03 -0500

    finished table creation sql query, added dev team to rds instance, created rds instance, created task list to work on next working time.

---
+++ sql_scripts/table_create.sql.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE "metrics" (
+    "metric_id" INTEGER   NOT NULL,
+    "product_id" INTEGER   NOT NULL,
+    "product_name" VARCHAR(50)   NOT NULL,
+    "date_time" DATETIME   NOT NULL,
+    "measured_weight" FLOAT   NOT NULL,
+    "measured_height" FLOAT   NOT NULL,
+    CONSTRAINT "pk_metrics" PRIMARY KEY (
+        "metric_id"
+     )
+);
+
+CREATE TABLE "products" (
+    "product_id" INTEGER   NOT NULL,
+    "product_name" VARCHAR(50)   NOT NULL,
+    "weight_min" FLOAT   NOT NULL,
+    "weight_max" FLOAT   NOT NULL,
+    "thick_min" FLOAT   NOT NULL,
+    "thick_max" FLOAT   NOT NULL,
+    CONSTRAINT "pk_products" PRIMARY KEY (
+        "product_id"
+     )
+);
+
+ALTER TABLE "metrics" ADD CONSTRAINT "fk_metrics_product_id" FOREIGN KEY("product_id")
+REFERENCES "products" ("product_id");
+
